69f1d2af96ee59db27ec611b99658dd12a0c1520
Matthieu B <66959271+mtthidoteu@users.noreply.github.com>
2023-02-08 09:45:57 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -23,12 +23,12 @@ VERSION = "1.1.0"
 
 
 def get_locale():
-    if request.args.get('lang'):
+     if os.getenv("FORCE_LANGUAGE"):
+        return os.getenv("FORCE_LANGUAGE")
+     elif request.args.get('lang'):
         session['lang'] = request.args.get('lang')
         return session.get('lang', 'en')
-    elif os.getenv("FORCE_LANGUAGE"):
-        return os.getenv("FORCE_LANGUAGE")
-    else:
+     else:
         return request.accept_languages.best_match(app.config['LANGUAGES'].keys())
 
 
