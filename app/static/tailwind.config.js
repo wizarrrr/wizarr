a59928ab01402601ec3374db14758b95a9d97892
Matthieu B <matthieu@jaseroque.com>
2023-02-24 18:04:01 +0000

    Refresh

--- app/static/tailwind.config.js
+++ app/static/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
+    colors: {
+      primary: '#fe4155',
+      primary_hover: '#982633',
+      secondary: '#533c5b',
+      secondary_hover: '#332538'
+      // ...
+    }
   },
   plugins: [
     require("flowbite/plugin")
