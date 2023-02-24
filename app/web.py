f90584845d0e75bfa2fcbdf07bbdd426a3659856
Matthieu B <matthieu@jaseroque.com>
2023-02-24 17:33:26 +0000

    better user experience

--- app/web.py
+++ app/web.py
@@ -156,19 +156,19 @@ def wizard(action):
 @app.errorhandler(500)
 def server_error(e):
     logging.error(e)
-    return render_template('500.html'), 500
+    return render_template('error/500.html'), 500
 
 
 @app.errorhandler(404)
 def server_error(e):
     logging.error(e)
-    return render_template('404.html'), 404
+    return render_template('error/404.html'), 404
 
 
 @app.errorhandler(401)
 def server_error(e):
     logging.error(e)
-    return render_template('401.html'), 401
+    return render_template('error/401.html'), 401
 
 
 @app.context_processor
