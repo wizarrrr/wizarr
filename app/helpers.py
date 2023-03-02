d4716f795fd9e50f9e01c81933578f7d1af12031
Matthieu B <matthieu@jaseroque.com>
2023-03-02 13:37:50 +0000

    Cleanup

--- app/helpers.py
+++ app/helpers.py
@@ -1,45 +1,6 @@
-from app import *
-from app.plex import *
-from app.jellyfin import *
-from app.ombi import *
-
-def GlobalDeleteUser(user):
-    server_type = Settings.get(Settings.key == "server_type").value
-    if server_type == "plex":
-        try:
-            deleteUser(user)
-        except Exception as e:
-            if "429" in str(e):
-                logging.error("Too many requests to Plex API")
-            else:
-                logging.error("Unable to delete user: " + str(e))
-    elif server_type == "jellyfin":
-        jf_DeleteUser(user)
-    Users.delete().where(Users.id == user).execute()
-    ombi_DeleteUser(user)
-    
+from app import Invitations
 
 
-def GlobalGetUsers():
-    server_type = Settings.get(Settings.key == "server_type").value
-    if server_type == "plex":
-        try:
-            users = getUsers()
-            return users
-        except Exception as e:
-            if "429" in str(e):
-                logging.error("Too many requests to Plex API")
-            else:
-                logging.error("Unable to get users: " + str(e))
-    elif server_type == "jellyfin":
-        try:
-            users = jf_GetUsers()
-            return users
-        except Exception as e:
-            if "429" in str(e):
-                logging.error("Too many requests to Jellyfin API")
-            else:
-                logging.error("Unable to get users: " + str(e))
 
 def isInviteValid(code):
         invitation = Invitations.get_or_none(Invitations.code == code)
