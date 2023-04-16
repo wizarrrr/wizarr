d0fc126ffed43c013cab32aea16c6788b8b3b8de
Damien Fajole <loupegame@gmail.com>
2023-04-16 17:07:11 +0200

    Change None to string

--- app/plex.py
+++ app/plex.py
@@ -79,7 +79,7 @@ def plex_get_users():
 def plex_delete_user(id):
     plex_get_users.cache_clear()
     email = Users.get(Users.id == id).email
-    if(email != None):
+    if(email != "None"):
         plex_token = Settings.get(Settings.key == "api_key").value
         admin = MyPlexAccount(plex_token)
         admin.removeHomeUser(email)
