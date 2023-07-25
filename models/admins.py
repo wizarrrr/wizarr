30fb5f9b37bc0835afe7d9e0b8a4d2295a7dd668
Ashley <admin@ashleybailey.me>
2023-07-25 19:42:24 +0100

    api update

--- models/admins.py
+++ models/admins.py
@@ -1,15 +1,4 @@
 from flask_restx import Model, fields
-from peewee import SQL, CharField, DateTimeField, IntegerField
-from .base import BaseModel
-
-
-class Admins(BaseModel):
-    id = IntegerField(primary_key=True)
-    username = CharField(unique=True)
-    password = CharField()
-    email = CharField(null=True, unique=True, default=None)
-    last_login = DateTimeField(null=True, default=None)
-    created = DateTimeField(constraints=[SQL("DEFAULT (datetime('now'))")])
 
 
 AdminsPostModel = Model('AdminsPostModel', {
