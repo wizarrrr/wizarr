b00ff837718a585c7076f40f54c1d3e1c9bcd26c
Ashley <admin@ashleybailey.me>
2023-07-25 21:46:51 +0100

    add backup

--- app/backup.py
+++ app/backup.py
@@ -1,144 +1,146 @@
-# import base64
-# import binascii
-# import json
-# import os
-# from datetime import datetime
+from models.database.base import db
+from app import app
+from cryptography.fernet import Fernet, InvalidToken
+from base64 import urlsafe_b64encode
+from flask import request, make_response
+from datetime import datetime
+from json import dumps, loads
 
-# from Crypto import Random
-# from Crypto.Cipher import AES
-# from Crypto.Protocol.KDF import PBKDF2
-# from cryptography.fernet import Fernet
-# from cryptography.hazmat.primitives import hashes
-# from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-# from flask import make_response, request
-# from playhouse.shortcuts import model_to_dict
+def backup_database():
 
-# from app import app
-# from models import all_models
+    # Backup dictionary
+    backup = {}
 
+    # Get all tables
+    db_tables = db.get_tables()
 
-# class DateTimeEncoder(json.JSONEncoder):
-#     def default(self, obj):
-#         if isinstance(obj, datetime):
-#             return obj.isoformat()
-#         return super().default(obj)
-    
-# class DateTimeDecoder(json.JSONDecoder):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(object_hook=self.object_hook, *args, **kwargs)
-        
-#     def object_hook(self, obj):
-#         for key, value in obj.items():
-#             if isinstance(value, str):
-#                 try:
-#                     obj[key] = datetime
-#                 except ValueError:
-#                     pass
-#         return obj
+    # Get all rows in the table
+    for table in db_tables:
 
-# def generate_fernet_key(password, salt):
-#     # Generate the Fernet key using PBKDF2HMAC
-#     kdf = PBKDF2HMAC(
-#         algorithm=hashes.SHA256(),
-#         length=32,  # Fernet key size is 32 bytes
-#         salt=salt,
-#         iterations=100_000  # Adjust the number of iterations as per your requirements
-#     )
-#     key = kdf.derive(password.encode())
+        # Add the table to the backup dictionary
+        backup[table] = []
 
-#     # Base64 encode the key
-#     key_b64 = base64.urlsafe_b64encode(key)
+        # Get all rows in the table
+        db_rows = db.execute_sql(f"SELECT * FROM {table}")
 
-#     # Return the Fernet key
-#     return key_b64
+        # Get all columns in the table
+        db_columns = [column[0] for column in db_rows.description]
 
+        # Add all rows to the backup dictionary
+        for row in db_rows:
+            backup[table].append(dict(zip(db_columns, row)))
 
-# def encrypt_data(password, data):
-#     # Generate a random salt
-#     salt = os.urandom(16)
+    return backup
 
-#     # Generate the Fernet key
-#     fernet_key = generate_fernet_key(password, salt)
+def generate_key(text):
+    # Convert the text to bytes
+    text = text.encode()
 
-#     # Create a Fernet cipher object using the derived key
-#     cipher = Fernet(fernet_key)
+    # Pad the text to 32 bytes
+    text = text + b"=" * (32 - len(text) % 32)
 
-#     # Convert the data to JSON string
-#     json_string = json.dumps(data, cls=DateTimeEncoder)
+    # Encode the text to base64
+    text = urlsafe_b64encode(text).decode()
 
-#     # Encrypt the JSON string using the Fernet cipher
-#     encrypted_data = cipher.encrypt(json_string.encode())
+    # Return the base64 encoded text
+    return text
 
-#     # Return the encrypted data and salt
-#     return encrypted_data, salt
+def encrypt_backup(backup: dict, key: str):
+    # Create the encryption key
+    key = Fernet(key)
+
+    def encrypt_string(string: str):
+        return key.encrypt(string.encode()).decode()
+
+    backup_string = dumps(backup)
+    encrypted_backup = encrypt_string(backup_string)
+
+    return encrypted_backup
 
 
-# def decrypt_data(password, encrypted_data, salt):
-#     # Generate the Fernet key
-#     fernet_key = generate_fernet_key(password, salt)
 
-#     # Create a Fernet cipher object using the derived key
-#     cipher = Fernet(fernet_key)
 
-#     # Decrypt the encrypted data
-#     decrypted_data = cipher.decrypt(encrypted_data)
+def decrypt_backup(backup: str, key: str):
+    # Create the encryption key
+    key = Fernet(key)
 
-#     # Convert the decrypted data from JSON string to a Python object
-#     data = json.loads(decrypted_data, cls=DateTimeDecoder)
+    def decrypt_string(string: str):
+        return key.decrypt(string).decode()
 
-#     # Return the decrypted data
-#     return data
+    backup_string = decrypt_string(backup)
 
-# def create_backup():
-#     # Create a dictionary to store the data from all models
-#     data = {}
-    
-#     # Loop through all models and store the data in the dictionary
-#     for model_class in all_models:
-#         table_name = model_class._meta.table_name
-#         records = model_class.select()
-        
-#         data[table_name] = [model_to_dict(record) for record in records]
-        
-#     return data
+    return loads(backup_string)
 
-# @app.route('/backup')
-# def backup():
-#     response = make_response(create_backup(), 200)
-#     response.headers["Content-Type"] = "application/json"
-    
-#     return response
 
-# @app.route('/backup/encrypted')
-# def backup_encrypted():
-#     # Create a backup
-#     backup = create_backup()
-    
-#     # Encrypt the backup    
-#     encrypted_backup, salt = encrypt_data(password="overseerr", data=backup)
-    
-#     response = make_response(encrypted_backup, 200)
-#     response.headers["Content-Type"] = "application/octet-stream"
-#     response.headers["Content-Disposition"] = "attachment; filename=" + str(salt) + ".backup"
-    
-#     return response
+@app.post("/backup/download")
+def backup_route():
 
-# # Decrypt a backup
-# @app.route('/backup/decrypt', methods=['POST'])
-# def decrypt_backup():
-#     # Get the encrypted backup file
-#     encrypted_backup = request.files['backup']
-    
-#     # Get the salt from the filename
-#     salt = encrypted_backup.filename.split(".")[0]
-    
-#     # Convert the salt from string to bytes
-#     salt = salt.encode()
-    
-#     # Decrypt the backup
-#     decrypted_backup = decrypt_data(password="overseerr", encrypted_data=encrypted_backup.read(), salt=salt)
-    
-#     response = make_response(decrypted_backup, 200)
-#     response.headers["Content-Type"] = "application/json"
-    
-#     return response
+    # Get the password from the form
+    password = request.form.get("password", None)
+
+    # If the password is None, return an error
+    if password is None:
+        raise ValueError("Password is required")
+
+    # Generate the key
+    key = generate_key(password)
+
+    # Encrypt the backup
+    data = None
+
+    try:
+        data = encrypt_backup(backup_database(), key)
+    except InvalidToken:
+        return { "error": "Invalid password" }
+    except Exception as e:
+        return { "error": str(e) }
+
+    if data is None:
+        return { "error": "An unknown error occurred" }
+
+    # Make a response as a download file
+    file_name = f"wizarr-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.backup"
+
+    response = make_response(data)
+    response.headers["Content-Disposition"] = f"attachment; filename={file_name}"
+
+    # Return the response
+    return response
+
+
+@app.post("/backup/restore")
+def decrypt_route():
+
+    # Get the posted file and password
+    backup_file = request.files["backup"]
+    password = request.form.get("password", None)
+
+    # Check if the file exists
+    if not backup_file:
+        raise FileNotFoundError("File not found")
+
+    # If the password is None, return an error
+    if password is None:
+        raise ValueError("Password is required")
+
+    # Decrypt the backup
+    data = None
+
+    try:
+        data = decrypt_backup(backup_file.read(), generate_key(password))
+    except InvalidToken:
+        return { "error": "Invalid password" }
+    except Exception as e:
+        return { "error": str(e) }
+
+    if data is None:
+        return { "error": "An unknown error occurred" }
+
+    # Restore the backup
+    return data
+
+
+
+@app.route("/test/<string:subpath>")
+def test_route(subpath):
+    return generate_key(subpath)
