867fdbc886bc4d0566a851deba468614bb538bdf
Ashley <admin@ashleybailey.me>
2023-09-24 23:57:40 +0100

    Add "@nanogiants/vue3-longpress" package and define new animation styles in tailwind.config.js
    - Add "@nanogiants/vue3-longpress" package to the project dependencies.
    - Define new animation styles for "wiggle" and "shake" in tailwind.config.js.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -16,6 +16,23 @@ module.exports = {
                 secondary: "#4B5563",
                 secondary_hover: "#39414b",
             },
+            animation: {
+                wiggle: "wiggle 0.5s ease-in-out infinite",
+                // animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+                shake: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
+            },
+            keyframes: {
+                wiggle: {
+                    "0%, 100%": { transform: "rotate(-0.25deg)" },
+                    "50%": { transform: "rotate(0.25deg)" },
+                },
+                shake: {
+                    "10%, 90%": { transform: "translate3d(-1px, 0, 0)" },
+                    "20%, 80%": { transform: "translate3d(2px, 0, 0)" },
+                    "30%, 50%, 70%": { transform: "translate3d(-4px, 0, 0)" },
+                    "40%, 60%": { transform: "translate3d(4px, 0, 0)" },
+                },
+            },
         },
     },
     plugins: [
