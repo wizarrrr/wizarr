41349f7df21460cc17f5bb45f59674f77575874c
Ashley <admin@ashleybailey.me>
2023-09-28 20:56:18 +0100

    Add Requests page to Settings module
    This commit adds a new file, Requests.vue, to the frontend/src/modules/settings/pages directory. The file contains the template and script for the Requests page, which displays a message indicating that it will be included in the next update.

---
+++ frontend/src/modules/settings/pages/Requests.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex justify-center">
+        <span class="text-gray-400">{{ __("Coming in the next update") }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "Requests",
+});
+</script>
