f7d8f5f45b5560ab989117b1879736cd0113b7cf
Ashley <admin@ashleybailey.me>
2023-09-28 20:53:38 +0100

    Delete unused files and functions in the codebase
    - Delete migrations.py, oauth_clients.py, partials.py, babel_locale.py, and check_enviroment.py
    - These files are no longer used in the codebase and can be safely deleted

--- backend/helpers/notifications.py
+++
@@ -1,9 +0,0 @@
-from app.models.database.notifications import Notifications
-
-
-def get_notifications():
-    # Get all notification agents from the database
-    notifications = Notifications.select()
-
-    # Return all notification agents
-    return notifications
