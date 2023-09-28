3923d313d6f81ea136c35feea723beb3b6f7d38b
Ashley <admin@ashleybailey.me>
2023-09-28 20:55:34 +0100

    feat: Add InvitationItem component
    - Added a new Vue component called "InvitationItem" to handle the display of invitation items in the invitation list.
    - The component renders a list item with various details of the invitation.
    - It includes buttons for copying the invitation code to the clipboard, sharing the invitation, and viewing the list of users who have used the invitation.
    - The component also handles the deletion of invitations.
    
    feat: Add InvitationList component
    
    - Added a new Vue component called "InvitationList" to display a list of invitations.
    - The component uses the "Draggable" component to enable dragging and reordering of the invitation items.
    - Each invitation item is rendered using the "InvitationItem" component.
    
    feat: Add InvitationUserList component
    
    - Added a new Vue component called "InvitationUserList" to display a list of users who have used an invitation.
    - The component uses the "SimpleUserList" component to render the list of users.
    - The list of users is passed as a prop to the component.

---
+++ frontend/src/modules/admin/components/Invitations/InvitationUserList.vue
@@ -0,0 +1,23 @@
+<template>
+    <SimpleUserList :users="users" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import type { Users } from "@/types/api/users";
+
+import SimpleUserList from "./SimpleUserList/SimpleUserList.vue";
+
+export default defineComponent({
+    name: "InvitationUserList",
+    components: {
+        SimpleUserList,
+    },
+    props: {
+        users: {
+            type: Array as () => Users,
+            required: true,
+        },
+    },
+});
+</script>
