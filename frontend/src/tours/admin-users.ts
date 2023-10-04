c72ef8796bbdf676704374edb9bac5e862f7cb37
Ashley <admin@ashleybailey.me>
2023-10-04 06:03:44 +0100

    feat: Add admin-users module for managing media server users
    This commit adds a new file `admin-users.ts` which contains the implementation for managing media server users in the admin panel. The file exports three functions: `steps`, `options`, and `callbacks` that provide customization options for the tour guide feature. The `steps` function defines the tour guide steps with localized titles and content for the user interface elements. The `options` function sets up the tour guide options, including the finish label. The `callbacks` function defines a callback function to be triggered when the tour guide finishes, which redirects the user to the next page in the app (admin settings).

---
+++ frontend/src/tours/admin-users.ts
@@ -0,0 +1,33 @@
+import type { CustomTourGuideStep, TourGuideCallbacks } from ".";
+
+import type { App } from "vue";
+import type TourGuideOptions from "@sjmc11/tourguidejs/src/core/options";
+
+const steps = (__: (key: string) => string): CustomTourGuideStep[] => [
+    {
+        title: __("Your Users"),
+        content: __("All of your media server users will appear here in a list. You can manage them, edit them, and delete them. Other information like their expiration or creation date will also be displayed here."),
+        target: document.querySelector("#userList > li:nth-child(1)") ?? "#userList",
+    },
+    {
+        title: __("Scan for Users"),
+        content: __("Wizarr will automatically scan your media server for new users, but you can also manually scan for new users by clicking on the 'Scan for Users' button, this is useful if Wizarr has not gotten around to doing it yet."),
+        target: "#scanUsers",
+    },
+];
+
+const options = (__: (key: string) => string, app?: App): Partial<TourGuideOptions> => {
+    return {
+        finishLabel: __("Next Page"),
+    };
+};
+
+const callbacks = (__: (key: string) => string, app?: App): TourGuideCallbacks => {
+    return {
+        onFinish: () => {
+            app?.config.globalProperties.$router.push("/admin/settings");
+        },
+    };
+};
+
+export { steps, options, callbacks };
