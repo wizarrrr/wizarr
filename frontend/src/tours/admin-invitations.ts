30c4efe008bb72c493d6f819dcb8ea633425af95
Ashley <admin@ashleybailey.me>
2023-10-04 06:04:11 +0100

    Add admin-invitations to manage user invitations in the frontend
    - Add admin-invitations.ts file to handle the management of user invitations
    - Define custom tour guide steps for managing invitations
    - Configure options for the tour guide, including the finish label
    - Implement callbacks for the tour guide, including redirecting to the user management page on finish

---
+++ frontend/src/tours/admin-invitations.ts
@@ -0,0 +1,33 @@
+import type { CustomTourGuideStep, TourGuideCallbacks } from ".";
+
+import type { App } from "vue";
+import type TourGuideOptions from "@sjmc11/tourguidejs/src/core/options";
+
+const steps = (__: (key: string) => string): CustomTourGuideStep[] => [
+    {
+        title: __("Your Invitations"),
+        content: __("This is where you can manage your invitations, they will appear here in a list. Invitations are used to invite new users to your media server."),
+        target: document.querySelector("#invitationList > li:nth-child(1)") ?? "#invitationList",
+    },
+    {
+        title: __("Create Invitation"),
+        content: __("You can create a new invitation by clicking on the 'Create Invitation' button."),
+        target: "#createInvitation",
+    },
+];
+
+const options = (__: (key: string) => string, app?: App): Partial<TourGuideOptions> => {
+    return {
+        finishLabel: __("Next Page"),
+    };
+};
+
+const callbacks = (__: (key: string) => string, app?: App): TourGuideCallbacks => {
+    return {
+        onFinish: () => {
+            app?.config.globalProperties.$router.push("/admin/users");
+        },
+    };
+};
+
+export { steps, options, callbacks };
