121a618e93c2162f56408461294f16de688212c2
Ashley <admin@ashleybailey.me>
2023-10-04 06:12:38 +0100

    feat: Update i18n configuration and language selection
    Improve the i18n configuration by dynamically selecting the language based on the user's preferred languages. Log the preferred languages and the selected language for debugging. Also, update the available languages to use an object instead of hardcoding each language. Export the language and availableLanguages variables for external use.
    
    - Dynamically select the language based on the user's preferred languages
    - Log the preferred languages and the selected language for debugging
    - Update available languages to use an object instead of hardcoding each language
    - Export the language and availableLanguages variables for external use

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,33 +1,47 @@
-import { createGettext } from "vue3-gettext";
+import { createGettext, type GetTextOptions } from "vue3-gettext";
 import translations from "./language/translations.json";
 
+// Available languages
+const availableLanguages: GetTextOptions["availableLanguages"] = {
+    ca: "Català",
+    cs: "Čeština",
+    da: "Dansk",
+    de: "Deutsch",
+    en: "English",
+    es: "Español",
+    fa: "فارسی",
+    fr: "Français",
+    he: "עברית",
+    hr: "Hrvatski",
+    hu: "Magyar",
+    is: "Íslenska",
+    it: "Italiano",
+    lt: "Lietuvių",
+    nl: "Nederlands",
+    no: "Norsk",
+    pl: "Polski",
+    pt: "Português",
+    ro: "Română",
+    ru: "Русский",
+    sv: "Svenska",
+    zh_cn: "简体中文",
+    zh_tw: "繁體中文",
+};
+
+// Get the preferred languages
+const preferredLanguages = navigator.languages.map((language) => language.split("-")[0]);
+
+// Find the first available language that matches the preferred languages
+const language = preferredLanguages.find((language) => availableLanguages[language]) ?? "en";
+
+// Log the preferred languages
+console.log("Preferred languages:", preferredLanguages);
+console.log("Selected Language:", language);
+
+// Create the gettext instance
 const gettext = createGettext({
-    availableLanguages: {
-        ca: "Català",
-        cs: "Čeština",
-        da: "Dansk",
-        de: "Deutsch",
-        en: "English",
-        es: "Español",
-        fa: "فارسی",
-        fr: "Français",
-        he: "עברית",
-        hr: "Hrvatski",
-        hu: "Magyar",
-        is: "Íslenska",
-        it: "Italiano",
-        lt: "Lietuvių",
-        nl: "Nederlands",
-        no: "Norsk",
-        pl: "Polski",
-        pt: "Português",
-        ro: "Română",
-        ru: "Русский",
-        sv: "Svenska",
-        zh_cn: "简体中文",
-        zh_tw: "繁體中文",
-    },
-    defaultLanguage: "en",
+    availableLanguages,
+    defaultLanguage: language,
     translations: translations,
     globalProperties: {
         gettext: ["$gettext", "__"],
@@ -37,4 +51,5 @@ const gettext = createGettext({
     },
 });
 
+export { language, availableLanguages };
 export default gettext;
