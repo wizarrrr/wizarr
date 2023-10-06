db179f9858f2b83aacf8ca2f5d0ed471ae7d49a5
Ashley <admin@ashleybailey.me>
2023-10-06 05:40:29 +0100

    feat: Update Docker build platforms and manifest creation
    This commit updates the Docker build platforms and manifest creation process in order to support additional ARM64 platforms (linux/arm64/v7, linux/arm64/v8) alongside the existing platforms (linux/amd64, linux/arm64). The changes ensure that the Docker images are built for all necessary platforms and the multi-arch manifest is created and pushed correctly.
    
    Closes #123

--- scripts/docker-create.sh
+++ scripts/docker-create.sh
@@ -9,7 +9,7 @@ docker buildx create --name wizarrbuilder
 docker buildx use wizarrbuilder
 
 # Build the image using buildx
-docker buildx build --platform linux/amd64,linux/arm64 --cache-to type=local,dest=database/cache --push -t $IMAGE_NAME:$IMAGE_TAG --progress tty .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm64/v7,linux/arm64/v8 --cache-to type=local,dest=database/cache --push -t $IMAGE_NAME:$IMAGE_TAG --progress tty .
 
 # Remove the buildx environment
 docker buildx rm wizarrbuilder
