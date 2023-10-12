1ccf40bf1f4e9ae00d25579591b0490b206188af
Ashley <admin@ashleybailey.me>
2023-10-12 12:24:34 +0100

    refactor: Add support for multiple platforms in beta CI workflow
    This commit modifies the beta CI workflow to include support for multiple platforms, such as linux/386, linux/arm/v5, linux/arm/v7, and more. Additionally, it updates the docker-create script to build the image for these platforms and adds support for caching the build.

--- scripts/docker-create.sh
+++ scripts/docker-create.sh
@@ -9,7 +9,7 @@ docker buildx create --name wizarrbuilder
 docker buildx use wizarrbuilder
 
 # Build the image using buildx
-docker buildx build --platform linux/amd64,linux/arm64 --cache-to type=local,dest=database/cache --push -t $IMAGE_NAME:$IMAGE_TAG --progress tty .
+docker buildx build --platform linux/386,linux/amd64,linux/arm/v5,linux/arm/v7,linux/arm64/v8,linux/mips64le,linux/ppc64le,linux/s390x --cache-to type=local,dest=database/cache --push -t $IMAGE_NAME:$IMAGE_TAG -t $IMAGE_NAME:$IMAGE_TAG-3.4.2 --progress tty .
 
 # Remove the buildx environment
 docker buildx rm wizarrbuilder
