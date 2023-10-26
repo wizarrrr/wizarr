a449e911f43ba1b18848a19363fcf04de7a52551
Ashley <admin@ashleybailey.me>
2023-10-26 23:30:11 +0100

    Refactor RequestsItem.vue component, optimizing code and improving button styling.
    - Shortens button markup by removing unnecessary classes.
    - Replaces double quotes with single quotes for consistency.
    - Uses template literal to import SVG icon dynamically.
    
    This commit refactors the RequestsItem.vue component to optimize code and improve button styling. Changes include shortening button markup by removing unnecessary classes, replacing double quotes with single quotes for consistency, and using a template literal to import SVG icon dynamically.

--- apps/wizarr-frontend/src/modules/settings/components/Requests/RequestsItem.vue
+++ apps/wizarr-frontend/src/modules/settings/components/Requests/RequestsItem.vue
@@ -3,27 +3,16 @@
         <template #title>
             <span class="text-lg">{{ request.name }}</span>
             <p class="text-xs truncate text-gray-500 dark:text-gray-400 w-full">
-                {{ $filter('timeAgo', request.created) }}
+                {{ $filter("timeAgo", request.created) }}
             </p>
         </template>
         <template #buttons>
             <div class="flex flex-row space-x-2">
                 <!-- Edit Button -->
-                <FormKit
-                    type="button"
-                    data-theme="secondary"
-                    :disabled="true"
-                    :classes="{ input: '!bg-secondary !px-3.5 h-[36px]' }"
-                >
+                <FormKit type="button" data-theme="secondary" :disabled="true" :classes="{ input: '!bg-secondary !px-3.5 h-[36px]' }">
                     <i class="fa-solid fa-edit"></i>
                 </FormKit>
-                <FormKit
-                    type="button"
-                    data-theme="danger"
-                    :disabled="disabled.delete"
-                    @click="localDeleteAPIKey"
-                    :classes="{ input: '!bg-red-600 !px-3.5 h-[36px]' }"
-                >
+                <FormKit type="button" data-theme="danger" :disabled="disabled.delete" @click="localDeleteAPIKey" :classes="{ input: '!bg-red-600 !px-3.5 h-[36px]' }">
                     <i class="fa-solid fa-trash"></i>
                 </FormKit>
             </div>
@@ -32,15 +21,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { useRequestsStore } from '@/stores/requests';
-import { mapActions } from 'pinia';
+import { defineComponent } from "vue";
+import { useRequestsStore } from "@/stores/requests";
+import { mapActions } from "pinia";
 
-import ListItem from '@/components/ListItem.vue';
-import DefaultButton from '@/components/Buttons/DefaultButton.vue';
+import ListItem from "@/components/ListItem.vue";
+import DefaultButton from "@/components/Buttons/DefaultButton.vue";
 
 export default defineComponent({
-    name: 'RequestsItem',
+    name: "RequestsItem",
     components: {
         ListItem,
         DefaultButton,
@@ -56,28 +45,21 @@ export default defineComponent({
             disabled: {
                 delete: false,
             },
-            icon: '',
+            icon: "",
         };
     },
     methods: {
-        ...mapActions(useRequestsStore, ['deleteRequest']),
+        ...mapActions(useRequestsStore, ["deleteRequest"]),
         async localDeleteAPIKey() {
             this.disabled.delete = true;
-            this.$modal
-                .confirmModal(
-                    this.__('Are you sure?'),
-                    this.__('Are you sure you want to delete this request?'),
-                )
-                .then(async (result) => {
-                    if (result) await this.deleteRequest(this.request.id);
-                    this.disabled.delete = false;
-                });
+            this.$modal.confirmModal(this.__("Are you sure?"), this.__("Are you sure you want to delete this request?")).then(async (result) => {
+                if (result) await this.deleteRequest(this.request.id);
+                this.disabled.delete = false;
+            });
         },
     },
     async beforeCreate() {
-        const icon = await import(
-            `../../../../assets/img/logo/${this.request.service}.svg?raw`
-        );
+        const icon = await import(`../../../../assets/img/logo/${this.request.service}.svg?raw`);
         this.icon = icon.default;
     },
 });
