08cd565776d806e8795b8f60e318b0bc08b3a695
Ashley <admin@ashleybailey.me>
2023-10-26 23:28:53 +0100

    Refactor Discord component styling and use translation strings
    - Refactored the styling of the Discord component by updating the background color and scrollbar styles.
    - Updated the usage of translation strings to use double underscores and added localization for the "Members Online" and "Join our Discord" strings.
    - Removed the unused guild and invite data properties.
    - Added the "useServerStore" and "mapState" imports for managing the server settings.
    - Added a try-catch block for handling errors when loading the Discord Widget API.
    - Updated the "loadWidgetAPI" method to use the server's Discord ID from the state.
    - Adjusted the switch case in the "statusColor" method to use double quotes for consistency.

--- apps/wizarr-frontend/src/modules/help/components/Discord.vue
+++ apps/wizarr-frontend/src/modules/help/components/Discord.vue
@@ -1,32 +1,21 @@
 <template>
     <div class="my-[-2rem] mx-[-2rem]">
         <!-- Header -->
-        <div
-            class="flex justify-between items-center flex-row bg-[#5865F2] py-4 px-6"
-        >
+        <div class="flex justify-between items-center flex-row dark:bg-gray-700 py-4 px-6">
             <DiscordLogo />
             <div class="text-white text-sm">
                 <span class="font-bold">{{ totalMembersOnline }}</span>
-                {{ __('Members Online') }}
+                {{ __("Members Online") }}
             </div>
         </div>
 
         <!-- Members List -->
-        <div
-            class="flex flex-col space-y-2 h-[30vh] overflow-y-auto p-4 scrollbar-thin scrollbar-track-white dark:scrollbar-thumb-gray-700 dark:scrollbar-track-gray-800"
-        >
+        <div class="flex flex-col space-y-2 h-[30vh] overflow-y-auto p-4 scrollbar-thin scrollbar-track-white dark:scrollbar-thumb-gray-900 dark:scrollbar-track-gray-800">
             <template v-for="member in members">
                 <div class="flex items-center space-x-2">
                     <div class="relative w-[26px] h-[26px]">
-                        <img
-                            :src="member.avatar_url"
-                            class="w-[26px] h-[26px] rounded-full"
-                            alt="Avatar"
-                        />
-                        <span
-                            class="absolute bottom-[-2px] right-[-2px] w-3 h-3 rounded-full border-2 border-white dark:border-gray-800"
-                            :class="statusColor(member.status)"
-                        ></span>
+                        <img :src="member.avatar_url" class="w-[26px] h-[26px] rounded-full" alt="Avatar" />
+                        <span class="absolute bottom-[-2px] right-[-2px] w-3 h-3 rounded-full border-2 border-white dark:border-gray-800" :class="statusColor(member.status)"></span>
                     </div>
                     <span>{{ member.username }}</span>
                 </div>
@@ -34,27 +23,23 @@
         </div>
 
         <!-- Footer -->
-        <div
-            class="flex justify-between items-center flex-row bg-[#5865F2] py-4 px-6"
-        >
+        <div class="flex justify-between items-center flex-row dark:bg-gray-700 py-4 px-6">
             <div class="text-white text-sm">
-                {{ __('Join our Discord') }}
+                {{ __("Join our Discord") }}
             </div>
-            <a
-                :href="invite"
-                target="_blank"
-                rel="noopener noreferrer"
-                class="text-white text-sm"
-            >
-                {{ __('Join') }}
+            <a :href="invite" target="_blank" rel="noopener noreferrer" class="text-white text-sm">
+                {{ __("Join") }}
             </a>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import DiscordLogo from '@/assets/img/discord.svg?component';
+import { defineComponent } from "vue";
+import { useServerStore } from "@/stores/server";
+import { mapState } from "pinia";
+
+import DiscordLogo from "@/assets/img/discord.svg?component";
 
 export interface Member {
     id: string;
@@ -68,34 +53,29 @@ export interface Member {
 export type Members = Member[];
 
 export default defineComponent({
-    name: 'Discord',
+    name: "Discord",
     components: {
         DiscordLogo,
     },
     data() {
         return {
-            guild: '1020742926856372224',
             members: [] as Members,
-            invite: '',
+            invite: "",
             interval: null as unknown as NodeJS.Timeout,
         };
     },
     computed: {
         totalMembersOnline() {
-            return this.members.filter((member) => member.status === 'online')
-                .length;
+            return this.members.filter((member) => member.status === "online").length;
         },
+        ...mapState(useServerStore, ["settings"]),
     },
     methods: {
         async loadWidgetAPI(guild: string) {
             // Load the Discord Widget API
-            const response = await this.$axios
-                .get(`https://discord.com/api/guilds/${guild}/widget.json`)
-                .catch(() => {
-                    this.$toast.info(
-                        'Unable to load Discord Widget, this is most likely due to too many requests.',
-                    );
-                });
+            const response = await this.$axios.get(`https://discord.com/api/guilds/${guild}/widget.json`).catch(() => {
+                this.$toast.info("Unable to load Discord Widget, this is most likely due to too many requests.");
+            });
 
             // Validate the response
             if (!response) return;
@@ -106,21 +86,21 @@ export default defineComponent({
         },
         statusColor(status: string) {
             switch (status) {
-                case 'online':
-                    return 'bg-green-500';
-                case 'idle':
-                    return 'bg-yellow-500';
-                case 'dnd':
-                    return 'bg-red-500';
-                case 'offline':
-                    return 'bg-gray-500';
+                case "online":
+                    return "bg-green-500";
+                case "idle":
+                    return "bg-yellow-500";
+                case "dnd":
+                    return "bg-red-500";
+                case "offline":
+                    return "bg-gray-500";
                 default:
-                    return 'bg-gray-500';
+                    return "bg-gray-500";
             }
         },
     },
     mounted() {
-        this.loadWidgetAPI(this.guild);
+        this.loadWidgetAPI(this.settings.server_discord_id!);
     },
 });
 </script>
