011901667b8b90d2e30a7036e1ca0a88b33444de
Ashley <admin@ashleybailey.me>
2023-10-26 23:28:38 +0100

    Refactor Carousel.vue component
    - Simplify the template structure by removing unnecessary div elements.
    - Remove redundant classes and styles from the template.
    - Remove unused imports and interfaces.
    - Update the props structure for better type checking.
    - Optimize the methods for calculating carousel height.
    - Add event listeners for window resize and transitions.

--- apps/wizarr-frontend/src/components/Carousel.vue
+++ apps/wizarr-frontend/src/components/Carousel.vue
@@ -1,55 +1,19 @@
 <template>
-    <div
-        ref="carousel"
-        class="relative overflow-hidden rounded h-screen"
-        style="transition: max-height 0.5s ease-in-out"
-        :style="{ maxHeight: carouselHeight }"
-    >
+    <div ref="carousel" class="relative overflow-hidden rounded h-screen" style="transition: max-height 0.5s ease-in-out" :style="{ maxHeight: carouselHeight }">
         <template v-for="(view, index) in views" :key="index + 1">
             <div v-if="index == 0" :id="`carousel-item-${index}`"></div>
-            <div
-                :id="`carousel-item-${index + 1}`"
-                class="hidden duration-700 ease-in-out"
-            >
+            <div :id="`carousel-item-${index + 1}`" class="hidden duration-700 ease-in-out">
                 <section>
                     <div class="flex flex-col items-center justify-center">
                         <div class="relative w-full">
-                            <div
-                                class="text-gray-900 dark:text-white"
-                                :class="classes.wrapper"
-                            >
+                            <div class="text-gray-900 dark:text-white" :class="classes.wrapper">
                                 <template v-if="boxed">
-                                    <div
-                                        class="flex flex-col items-center justify-center md:container p-8 mx-auto"
-                                    >
-                                        <div
-                                            class="w-full md:w-1/2 lg:w-1/3 bg-white rounded shadow dark:border dark:bg-gray-800 dark:border-gray-700 overflow-hidden"
-                                        >
+                                    <div class="flex flex-col items-center justify-center md:container p-8 mx-auto">
+                                        <div class="w-full md:w-1/2 lg:w-1/3 bg-white rounded shadow dark:border dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
                                             <div class="relative">
-                                                <div
-                                                    class="text-gray-900 dark:text-white"
-                                                >
-                                                    <div
-                                                        class="block w-full"
-                                                        :class="classes.inner"
-                                                    >
-                                                        <component
-                                                            v-bind="
-                                                                _props[
-                                                                    view.name
-                                                                ] || view.props
-                                                            "
-                                                            @next="
-                                                                _carousel?.next()
-                                                            "
-                                                            @prev="
-                                                                _carousel?.prev()
-                                                            "
-                                                            @height="
-                                                                updateHeight()
-                                                            "
-                                                            :is="view.view"
-                                                        />
+                                                <div class="text-gray-900 dark:text-white">
+                                                    <div class="block w-full" :class="classes.inner">
+                                                        <component v-bind="_props[view.name] || view.props" @next="_carousel?.next()" @prev="_carousel?.prev()" @height="updateHeight()" :is="view.view" />
                                                     </div>
                                                 </div>
                                             </div>
@@ -57,13 +21,7 @@
                                     </div>
                                 </template>
                                 <template v-else>
-                                    <component
-                                        v-bind="_props[view.name] || view.props"
-                                        @next="_carousel?.next()"
-                                        @prev="_carousel?.prev()"
-                                        @height="updateHeight()"
-                                        :is="view.view"
-                                    />
+                                    <component v-bind="_props[view.name] || view.props" @next="_carousel?.next()" @prev="_carousel?.prev()" @height="updateHeight()" :is="view.view" />
                                 </template>
                             </div>
                         </div>
@@ -75,22 +33,23 @@
 </template>
 
 <script lang="ts">
-import { Carousel } from 'flowbite';
-import { defineComponent, markRaw } from 'vue';
-import { useEventListener } from '@vueuse/core';
+import { Carousel } from "flowbite";
+import { defineComponent, markRaw } from "vue";
+import { useEventListener } from "@vueuse/core";
 
-import type { Component } from 'vue';
-import type { CarouselInterface, CarouselItem } from 'flowbite';
+import type { Component } from "vue";
+import type { CarouselInterface, CarouselItem } from "flowbite";
 
-import DefaultButton from '@/components/Buttons/DefaultButton.vue';
-import DefaultLoading from '@/components/Loading/DefaultLoading.vue';
+import DefaultButton from "@/components/Buttons/DefaultButton.vue";
+import DefaultLoading from "@/components/Loading/DefaultLoading.vue";
 
-interface CarouselViewProps {
+export interface CarouselViewProps {
     views: Array<{
         name: string;
         url?: string;
         view: Component;
         props?: Record<string, any>;
+        disabled?: boolean;
     }>;
     currentView: number;
     callbacks: {
@@ -103,41 +62,41 @@ interface CarouselViewProps {
 }
 
 export default defineComponent({
-    name: 'Carousel',
+    name: "Carousel",
     components: {
         DefaultButton,
         DefaultLoading,
     },
     props: {
         boxed: {
-            type: Boolean as () => CarouselViewProps['boxed'],
+            type: Boolean as () => CarouselViewProps["boxed"],
             default: false,
             required: false,
         },
         classes: {
-            type: Object as () => CarouselViewProps['classes'],
+            type: Object as () => CarouselViewProps["classes"],
             default: () => ({
-                wrapper: 'px-6 pb-6 sm:px-8 sm:pb-8',
-                inner: '',
+                wrapper: "px-6 pb-6 sm:px-8 sm:pb-8",
+                inner: "",
             }),
             required: false,
         },
         views: {
-            type: Array as () => CarouselViewProps['views'],
+            type: Array as () => CarouselViewProps["views"],
             required: true,
         },
         currentView: {
-            type: Number as () => CarouselViewProps['currentView'],
+            type: Number as () => CarouselViewProps["currentView"],
             default: 1,
             required: false,
         },
         callbacks: {
-            type: Object as () => CarouselViewProps['callbacks'],
+            type: Object as () => CarouselViewProps["callbacks"],
             default: () => ({}),
             required: false,
         },
         props: {
-            type: Object as () => CarouselViewProps['props'],
+            type: Object as () => CarouselViewProps["props"],
             default: () => ({}),
             required: false,
         },
@@ -149,19 +108,16 @@ export default defineComponent({
             _currentView: this.currentView,
             _transitioning: false,
             _callbacks: this.callbacks,
-            _props: this.props as Partial<
-                Record<(typeof this.views)[number]['name'], Record<string, any>>
-            >,
+            _props: this.props as Partial<Record<(typeof this.views)[number]["name"], Record<string, any>>>,
         };
     },
     methods: {
         getCarouselHeight(): string {
             const activeItem = this._carousel?._activeItem?.el;
-            if (!activeItem) return '0px';
+            if (!activeItem) return "0px";
 
-            const activeItemChild = activeItem.firstChild
-                ?.firstChild as HTMLElement | null;
-            if (!activeItemChild) return '0px';
+            const activeItemChild = activeItem.firstChild?.firstChild as HTMLElement | null;
+            if (!activeItemChild) return "0px";
 
             return `${activeItemChild.offsetHeight}px`;
         },
@@ -175,12 +131,9 @@ export default defineComponent({
             if (!carouselRef) return;
 
             const carouselTransition = carouselRef.style.transition;
-            carouselRef.style.transition = 'none';
+            carouselRef.style.transition = "none";
             carouselRef.style.maxHeight = this.getCarouselHeight();
-            setTimeout(
-                () => (carouselRef.style.transition = carouselTransition),
-                500,
-            );
+            setTimeout(() => (carouselRef.style.transition = carouselTransition), 500);
         },
         onChange(carousel: CarouselInterface) {
             // Update current view index
@@ -209,15 +162,13 @@ export default defineComponent({
         _currentView: {
             immediate: true,
             handler() {
-                this.$emit('current', this._currentView);
+                this.$emit("current", this._currentView);
             },
         },
     },
     async mounted() {
         // Get carousel items and map them to an array of objects
-        const carouselViews = (
-            this.$refs.carousel as HTMLElement
-        ).querySelectorAll('div[id^="carousel-item"]');
+        const carouselViews = (this.$refs.carousel as HTMLElement).querySelectorAll('div[id^="carousel-item"]');
         const carouselItems = Array.from(carouselViews).map((view, index) => ({
             el: view,
             position: index,
@@ -233,21 +184,13 @@ export default defineComponent({
         (window as any)._carousel = this._carousel;
 
         // // Add event listeners for window resize
-        useEventListener(window, 'resize', this.handleWindowResize);
-        useEventListener(
-            this.$refs._carousel as HTMLElement,
-            'transitionend',
-            () => setTimeout(() => (this._transitioning = false), 500),
-        );
-        useEventListener(
-            this.$refs._carousel as HTMLElement,
-            'transitionstart',
-            () => (this._transitioning = true),
-        );
+        useEventListener(window, "resize", this.handleWindowResize);
+        useEventListener(this.$refs._carousel as HTMLElement, "transitionend", () => setTimeout(() => (this._transitioning = false), 500));
+        useEventListener(this.$refs._carousel as HTMLElement, "transitionstart", () => (this._transitioning = true));
     },
     beforeUnmount() {
         // Remove event listeners for window resize
-        window.removeEventListener('resize', this.handleWindowResize);
+        window.removeEventListener("resize", this.handleWindowResize);
     },
 });
 </script>
