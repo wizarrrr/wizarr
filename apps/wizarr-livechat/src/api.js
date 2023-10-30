02d0363f711e7f35a7ccf31ae24ec353a71f2953
Ashley <admin@ashleybailey.me>
2023-10-30 04:01:05 +0000

    delete livechat

--- apps/wizarr-livechat/src/api.js
+++
@@ -1,6 +0,0 @@
-import LivechatClient from '@rocket.chat/sdk/lib/clients/Livechat';
-
-const host = 'https://chat.wizarr.dev';
-export const useSsl = host && host.match(/^https:/) !== null;
-
-export const Livechat = new LivechatClient({ host, protocol: 'ddp', useSsl });
