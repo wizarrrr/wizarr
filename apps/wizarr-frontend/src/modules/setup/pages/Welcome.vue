d55c87718f48882ae5a9fe6e8772cad16379a10e
Jam <1347620+JamsRepos@users.noreply.github.com>
2023-10-30 00:49:48 +0000

    refactor: :pencil2: Removed emby from the welcome page.
    Seeing as it's no longer natively supported, it's best we don't mention it having support.

--- apps/wizarr-frontend/src/modules/setup/pages/Welcome.vue
+++ apps/wizarr-frontend/src/modules/setup/pages/Welcome.vue
@@ -10,7 +10,7 @@
             <div class="text-gray-500 dark:text-gray-400">
                 <h2 class="text-xs">
                     Automatic User Invitation System for<br />
-                    Plex, Jellyfin, and Emby
+                    Plex and Jellyfin
                 </h2>
             </div>
 
@@ -19,7 +19,7 @@
             <!-- Description -->
             <p class="text-base text-center">
                 Wizarr is a powerful tool that makes it easy to manage user
-                invitations for your Plex, Jellyfin, or Emby media server. With
+                invitations for your Plex or Jellyfin media server. With
                 Wizarr, you can create unique invitation links to share with
                 users, and they will be automatically invited to your server
                 once they click on the link.
