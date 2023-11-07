c90bc4d049756b8af6cffad5b68af880bc019ed4
Ashley <admin@ashleybailey.me>
2023-11-07 23:26:09 +0000

    style(commitlint): remove usless text from config for commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,11 +6,6 @@ module.exports = {
         'footer-leading-blank': [1, 'always'],
         'footer-max-line-length': [2, 'always', 100],
         'header-max-length': [2, 'always', 100],
-        // 'subject-case': [
-        //     2,
-        //     'never',
-        //     ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-        // ],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
         'type-case': [2, 'always', 'lower-case'],
