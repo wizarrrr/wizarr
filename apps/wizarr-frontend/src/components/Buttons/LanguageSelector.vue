6f239dd3190f42f268e9ee5ea0bded0c6b0836e8
Jam <1347620+JamsRepos@users.noreply.github.com>
2023-11-07 16:34:34 +0000

    feat: :art: Added ability to specify classes to Language Selector & Theme Toggle.

--- apps/wizarr-frontend/src/components/Buttons/LanguageSelector.vue
+++ apps/wizarr-frontend/src/components/Buttons/LanguageSelector.vue
@@ -1,11 +1,12 @@
 <template>
     <button
-        class="text-gray-500 dark:text-gray-400 focus:outline-none text-sm"
-        type="button"
-        @click="createModal"
+    class="text-gray-500 dark:text-gray-400 focus:outline-none text-sm"
+    type="button"
+    @click="createModal"
     >
         <div
-            class="w-6 h-6 flex items-center justify-center rounded hover:bg-gray-200 hover:dark:bg-gray-700"
+            :class="iconClasses"
+            class="flex items-center justify-center rounded hover:bg-gray-200 hover:dark:bg-gray-700"
         >
             <i class="fa-solid fa-md fa-globe"></i>
         </div>
@@ -20,6 +21,17 @@ export default defineComponent({
     name: 'LanguageSelector',
     components: {
         LanguageModal,
+    },
+    props: {
+        iconClasses: {
+            type: String,
+            default: 'w-6 h-6',
+        },
+    },
+    computed: {
+        iconClasses(): string {
+            return this.iconClasses;
+        },
     },
     methods: {
         createModal() {
