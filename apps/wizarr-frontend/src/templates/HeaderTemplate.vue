e0b3f4d2d4fdba51ea36fcd0e32f27a74a2b2c17
Ashley <admin@ashleybailey.me>
2023-11-08 20:09:06 +0000

    fix: 🚑 Refactor AdminTemplate and HeaderTemplate components

--- apps/wizarr-frontend/src/templates/HeaderTemplate.vue
+++ apps/wizarr-frontend/src/templates/HeaderTemplate.vue
@@ -2,30 +2,18 @@
     <!-- Wrapper -->
     <div class="absolute top-0 left-0 bottom-0 right-0 mt-[64px] mb-[33px]">
         <!-- Border Box -->
-        <div
-            class="w-full rounded bg-none dark:border-gray-700 light:border-gray-200"
-            :class="class"
-        >
+        <div class="w-full rounded bg-none dark:border-gray-700 light:border-gray-200" :class="class">
             <!-- Header -->
             <!-- max-w-screen-xl flex flex-col md:flex-row items-start md:items-center justify-between mx-auto p-4 -->
-            <div
-                v-if="header || subheader || headerSlotAvailable"
-                class="flex items-center justify-center bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600 h-[75px]"
-            >
-                <div
-                    class="w-full max-w-screen-xl flex flex-row justify-between p-4"
-                >
+            <div v-if="header || subheader || headerSlotAvailable" class="flex items-center justify-center bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600 h-[75px]">
+                <div class="w-full max-w-screen-xl flex flex-row justify-between p-4">
                     <div class="flex items-center">
                         <div class="flex flex-col justify-start">
-                            <div
-                                class="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white"
-                            >
-                                {{ header ?? '' }}
+                            <div class="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
+                                {{ header ?? "" }}
                             </div>
-                            <div
-                                class="text-sm font-semibold leading-tight tracking-tight text-gray-900 dark:text-gray-400"
-                            >
-                                {{ subheader ?? '' }}
+                            <div class="text-sm font-semibold leading-tight tracking-tight text-gray-900 dark:text-gray-400">
+                                {{ subheader ?? "" }}
                             </div>
                         </div>
                     </div>
@@ -41,10 +29,7 @@
             </div>
 
             <!-- Footer -->
-            <div
-                v-if="footerSlotAvailable || footerActionsSlotAvailable"
-                class="flex items-center justify-between mb-6 md:mb-0 px-6 md:px-0"
-            >
+            <div v-if="footerSlotAvailable || footerActionsSlotAvailable" class="flex items-center justify-between mb-6 md:mb-0 px-6 md:px-0">
                 <div class="flex items-center">
                     <slot name="footer" />
                 </div>
@@ -57,10 +42,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'Box',
+    name: "Box",
     props: {
         header: {
             type: String,
@@ -70,7 +55,7 @@ export default defineComponent({
         },
         class: {
             type: String,
-            default: '',
+            default: "",
         },
     },
     computed: {
