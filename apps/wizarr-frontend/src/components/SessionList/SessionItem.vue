1b1eb766ce814393ab6f2723662abe8ab5aeb858
Ashley <admin@ashleybailey.me>
2023-11-08 05:44:42 +0000

    fix: 🩹 Fix session list item layout and add drag and drop functionality

--- apps/wizarr-frontend/src/components/SessionList/SessionItem.vue
+++ apps/wizarr-frontend/src/components/SessionList/SessionItem.vue
@@ -6,16 +6,13 @@
                 {{ session.ip }}
             </p>
             <p class="text-xs truncate text-gray-500 dark:text-gray-400 w-full">
-                {{ $filter('timeAgo', session.created) }}
+                {{ $filter("timeAgo", session.created) }}
             </p>
         </template>
 
         <template #buttons>
             <div class="flex flex-row space-x-2">
-                <button
-                    @click="deleteLocalSession"
-                    class="bg-red-600 hover:bg-primary_hover focus:outline-none text-white font-medium rounded px-3.5 py-2 text-sm dark:bg-red-600 dark:hover:bg-primary_hover"
-                >
+                <button @click="deleteLocalSession" class="bg-red-600 hover:bg-primary_hover focus:outline-none text-white font-medium rounded px-3.5 py-2 text-sm dark:bg-red-600 dark:hover:bg-primary_hover">
                     <i class="fa-solid fa-trash"></i>
                 </button>
             </div>
@@ -24,18 +21,18 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { formatTimeAgo } from '@vueuse/core';
-import { useSessionsStore } from '@/stores/sessions';
-import { mapActions } from 'pinia';
+import { defineComponent } from "vue";
+import { formatTimeAgo } from "@vueuse/core";
+import { useSessionsStore } from "@/stores/sessions";
+import { mapActions } from "pinia";
 
-import type { Session } from '@/types/api/sessions';
+import type { Session } from "@/types/api/sessions";
 
-import ListItem from '../ListItem.vue';
-import browserDetect from 'browser-detect';
+import ListItem from "../ListItem.vue";
+import browserDetect from "browser-detect";
 
 export default defineComponent({
-    name: 'SessionItem',
+    name: "SessionItem",
     components: {
         ListItem,
     },
@@ -48,24 +45,18 @@ export default defineComponent({
     computed: {
         browser(): string {
             const browser = browserDetect(this.session.user_agent);
-            return this.$filter(
-                'firstLetterUppercase',
-                browser.name ?? 'Unknown',
-            );
+            return this.$filter("firstLetterUppercase", browser.name ?? "Unknown");
         },
         os(): string {
             const browser = browserDetect(this.session.user_agent);
-            return this.$filters(
-                ['removeVersion', 'firstLetterUppercase'],
-                browser.os ?? 'Unknown',
-            );
+            return this.$filters(["removeVersion", "firstLetterUppercase"], browser.os ?? "Unknown");
         },
     },
     methods: {
         async deleteLocalSession() {
             await this.deleteSession(this.session.id);
         },
-        ...mapActions(useSessionsStore, ['deleteSession']),
+        ...mapActions(useSessionsStore, ["deleteSession"]),
     },
 });
 </script>
