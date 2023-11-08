e3c90fb40d94f1479decf8229223854986fea7df
Ashley <admin@ashleybailey.me>
2023-11-08 04:57:19 +0000

    fix: 🐛 fix linting

--- apps/wizarr-frontend/src/components/Footers/AdminFooter.vue
+++ apps/wizarr-frontend/src/components/Footers/AdminFooter.vue
@@ -7,16 +7,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'DefaultFooter',
+    name: "DefaultFooter",
     data() {
         return {
-            APP_NAME: '',
-            APP_VERSION: '',
-            APP_RELEASED: '',
-            APP_GITHUB_URL: '',
+            APP_NAME: "",
+            APP_VERSION: "",
+            APP_RELEASED: "",
+            APP_GITHUB_URL: "",
         };
     },
 });
--- apps/wizarr-frontend/src/components/FormKit/ButtonInput.vue
+++ apps/wizarr-frontend/src/components/FormKit/ButtonInput.vue
@@ -1,38 +1,20 @@
 <template>
     <div class="flex flex-row">
         <div class="relative w-full">
-            <div
-                v-html="context._rawPrefixIcon"
-                class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none"
-            ></div>
-            <input
-                v-bind="attrs"
-                :value="context._value"
-                @keydown="listenEnter"
-                @blur="context.handlers.blur"
-                @touch="context.handlers.touch"
-                @input="context.handlers.DOMInput"
-                :class="inputClasses"
-                class="h-[42px] w-full"
-            />
+            <div v-html="context._rawPrefixIcon" class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none"></div>
+            <input v-bind="attrs" :value="context._value" @keydown="listenEnter" @blur="context.handlers.blur" @touch="context.handlers.touch" @input="context.handlers.DOMInput" :class="inputClasses" class="h-[42px] w-full" />
         </div>
-        <FormKit
-            type="button"
-            :disabled="buttonDisabled"
-            @click="handleButton"
-            :class="context.classes.button"
-            :classes="buttonClasses"
-        >
+        <FormKit type="button" :disabled="buttonDisabled" @click="handleButton" :class="context.classes.button" :classes="buttonClasses">
             <component :is="context.slots.default" />
         </FormKit>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'ButtonInput',
+    name: "ButtonInput",
     props: {
         context: {
             type: Object as () => Record<string, any> & {
@@ -47,30 +29,26 @@ export default defineComponent({
         attrs() {
             return {
                 id: this.context.id,
-                type: 'text',
+                type: "text",
                 ...this.context.attrs,
             };
         },
         inputClasses() {
             return {
-                ...this.context.classes.input
-                    .split(' ')
-                    .reduce((acc: Record<string, boolean>, cur: string) => {
-                        acc[cur] = true;
-                        return acc;
-                    }, {}),
-                'pl-10': this.context.prefixIcon,
+                ...this.context.classes.input.split(" ").reduce((acc: Record<string, boolean>, cur: string) => {
+                    acc[cur] = true;
+                    return acc;
+                }, {}),
+                "pl-10": this.context.prefixIcon,
             };
         },
         buttonClasses() {
             return {
-                ...this.context.classes.button
-                    .split(' ')
-                    .reduce((acc: Record<string, boolean>, cur: string) => {
-                        acc[cur] = true;
-                        return acc;
-                    }, {}),
-                input: '!rounded-[0px] !rounded-r absolute inset-y-0 right-0 flex items-center px-2 h-[42px]',
+                ...this.context.classes.button.split(" ").reduce((acc: Record<string, boolean>, cur: string) => {
+                    acc[cur] = true;
+                    return acc;
+                }, {}),
+                input: "!rounded-[0px] !rounded-r absolute inset-y-0 right-0 flex items-center px-2 h-[42px]",
             };
         },
     },
@@ -81,7 +59,7 @@ export default defineComponent({
     },
     methods: {
         listenEnter(e: KeyboardEvent) {
-            if (e.key === 'Enter') {
+            if (e.key === "Enter") {
                 this.context.attrs.onButton();
             }
         },
