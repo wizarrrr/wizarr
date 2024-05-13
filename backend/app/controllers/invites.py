32cb95ec70cf2f2f8d2014866c80700bcc4eb8f7
WardPearce <wardpearce@pm.me>
2024-05-13 20:04:20 +1200

    Added service endpoints

--- backend/app/controllers/invites.py
+++ backend/app/controllers/invites.py
@@ -1,12 +1,16 @@
-from helpers.invites import Invite
-from litestar import Controller, Router, post
+from litestar import Controller, Router, get, post
 
-from app.models.invite import InviteAddModel
+from app.helpers.invites import Invite, create_invite
+from app.models.invite import CreateInviteModel, InviteAddModel, InviteModel
 from app.state import State
 
 
 class InviteController(Controller):
     path = "/{invite_id:str}"
+
+    @get("/")
+    async def invite_get(self, state: State, invite_id: str) -> InviteModel:
+        return await Invite(state, invite_id).get()
 
     @post("/{code:str}")
     async def invite_add(
@@ -15,6 +19,14 @@ class InviteController(Controller):
         await Invite(state, invite_id).add(data, code)
 
 
-routes = Router("/invites", tags=["invites"], route_handlers=[InviteController])
+@post("/")
+async def post_create_invite(state: State, create: CreateInviteModel) -> InviteModel:
+    invite, _ = await create_invite(state, create)
+    return invite
+
+
+routes = Router(
+    "/invites", tags=["invites"], route_handlers=[InviteController, post_create_invite]
+)
 
 __all__ = ["routes"]
--- backend/app/controllers/services.py
+++ backend/app/controllers/services.py
@@ -1,11 +1,37 @@
-from litestar import Controller, Router, get, post
+from litestar import Controller, Router, delete, get, patch, post
+from models.services.base import (
+    CreateServiceApiModel,
+    ServiceApiModel,
+    ServiceApiUpdateModel,
+)
 
-from app.helpers.services import Service
+from app.helpers.services import Service, create_service
 from app.state import State
 
 
 class ServiceController(Controller):
     path = "/{service_id:str}"
+
+    @get("/")
+    async def get_service(self, state: State, service_id: str) -> ServiceApiModel:
+        return (await Service(state, service_id).get()).details
+
+    @delete("/")
+    async def delete_service(self, state: State, service_id: str) -> None:
+        await Service(state, service_id).delete()
+
+    @patch("/")
+    async def update_service(
+        self, state: State, update: ServiceApiUpdateModel, service_id: str
+    ) -> None:
+        await Service(state, service_id).update(update)
+
+
+@post("/")
+async def post_create_service(
+    state: State, data: CreateServiceApiModel
+) -> ServiceApiModel:
+    return (await create_service(state, data)).details
 
 
 routes = Router(
