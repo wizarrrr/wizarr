f397e5dbbf848b4e7fad08caa8f508e097a375a0
Albin Medoc <stamline203@gmail.com>
2024-07-20 00:44:20 +0200

    refactor: 📦 Cleanup code for darkmode

--- apps/wizarr-frontend/src/components/Buttons/ThemeToggle.vue
+++ apps/wizarr-frontend/src/components/Buttons/ThemeToggle.vue
@@ -2,9 +2,9 @@
     <VTooltip>
         <button class="text-gray-500 dark:text-gray-400 focus:outline-none text-sm" type="button" @click="toggleTheme">
             <div :class="iconClasses" class="flex items-center justify-center rounded hover:bg-gray-200 hover:dark:bg-gray-700">
-                <i v-if="theme == DARK_VALUE" class="fa-solid fa-md fa-cloud-moon"></i>
-                <i v-if="theme == LIGHT_VALUE" class="fa-solid fa-md fa-sun"></i>
-                <i v-if="theme == SYSTEM_VALUE" class="fa-solid fa-md fa-desktop"></i>
+                <i v-if="theme == DARKMODE" class="fa-solid fa-md fa-cloud-moon"></i>
+                <i v-if="theme == LIGHTMODE" class="fa-solid fa-md fa-sun"></i>
+                <i v-if="theme == SYSTEMMODE" class="fa-solid fa-md fa-desktop"></i>
             </div>
         </button>
 
@@ -14,20 +14,19 @@
     </VTooltip>
 </template>
 
-
 <script lang="ts">
 import { defineComponent } from "vue";
 import { mapState, mapActions } from "pinia";
 import { useThemeStore } from "@/stores/theme";
-import { LIGHT_VALUE, DARK_VALUE, SYSTEM_VALUE } from "@/ts/utils/darkMode";
+import { LIGHTMODE, DARKMODE, SYSTEMMODE } from "@/ts/utils/darkMode";
 
 export default defineComponent({
     name: "ThemeToggle",
     data() {
         return {
-            LIGHT_VALUE,
-            DARK_VALUE,
-            SYSTEM_VALUE,
+            LIGHTMODE,
+            DARKMODE,
+            SYSTEMMODE,
         };
     },
     props: {
