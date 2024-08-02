739ad79b9e1c79ece2d54c72c9a08447ddf63d7d
albinmedoc <stamline203@gmail.com>
2024-08-02 11:17:06 +0200

    feat: 🎊 Update onboarding store to handle fixed onboarding pages

--- apps/wizarr-frontend/src/stores/onboarding.ts
+++ apps/wizarr-frontend/src/stores/onboarding.ts
@@ -1,8 +1,21 @@
 import { defineStore } from 'pinia';
+import { useServerStore } from "@/stores/server";
+import type { FixedOnboardingPage } from '@/types/api/onboarding/FixedOnboardingPage';
 import type { OnboardingPage } from '@/types/api/onboarding/OnboardingPage';
+
+export enum FixedOnboardingPageType {
+    WelcomePlex = 0,
+    DownloadPlex = 1,
+    WelcomeJellyfin = 2,
+    DownloadJellyfin = 3,
+    WelcomeEmby = 4,
+    DownloadEmby = 5,
+    Request = 6,
+}
 
 // Define the shape of the state in this store
 interface OnboardingStoreState {
+    fixedOnboardingPages: FixedOnboardingPage[];
     onboardingPages: OnboardingPage[];
 }
 
@@ -10,11 +23,46 @@ interface OnboardingStoreState {
 export const useOnboardingStore = defineStore('onboarding', {
     // Define the initial state of the store
     state: (): OnboardingStoreState => ({
+        fixedOnboardingPages: [],
         onboardingPages: [],
     }),
     getters: {
         enabledOnboardingPages(state) {
             return state.onboardingPages.filter(page => page.enabled);
+        },
+        enabledFixedOnboardingPages(state) {
+            const serverStore = useServerStore();
+            const pages = state.fixedOnboardingPages;
+
+            const filteredPages = [];
+            if (serverStore.settings.server_type == "plex") {
+                const welcomePage = pages.find((page) => page.id === FixedOnboardingPageType.WelcomePlex);
+                if(welcomePage) filteredPages.push(welcomePage);
+                const downloadPage = pages.find((page) => page.id === FixedOnboardingPageType.DownloadPlex);
+                if(downloadPage) filteredPages.push(downloadPage);
+            }
+
+            else if (serverStore.settings.server_type == "jellyfin") {
+                const welcomePage = pages.find((page) => page.id === FixedOnboardingPageType.WelcomeJellyfin);
+                if(welcomePage) filteredPages.push(welcomePage);
+                const downloadPage = pages.find((page) => page.id === FixedOnboardingPageType.DownloadJellyfin);
+                if(downloadPage) filteredPages.push(downloadPage);
+            }
+
+            else if (serverStore.settings.server_type == "emby") {
+                const welcomePage = pages.find((page) => page.id === FixedOnboardingPageType.WelcomeEmby);
+                if(welcomePage) filteredPages.push(welcomePage);
+                const downloadPage = pages.find((page) => page.id === FixedOnboardingPageType.DownloadEmby);
+                if(downloadPage) filteredPages.push(downloadPage);
+            }
+
+            if (!!serverStore.requests.length) {
+                const requestPage = pages.find((page) => page.id === FixedOnboardingPageType.Request);
+                if(requestPage) filteredPages.push(requestPage);
+            }
+
+            return filteredPages.sort((a, b) => a.id - b.id);
+        },
         onboardingVariables: () => {
             const serverStore = useServerStore();
             return {
@@ -29,19 +77,19 @@ export const useOnboardingStore = defineStore('onboarding', {
     actions: {
         // Asynchronously fetches onboarding pages from the server and updates the state
         async getOnboardingPages() {
-            const response = await this.$axios
-                .get<OnboardingPage, { data: OnboardingPage[] }>('/api/onboarding')
+            const [fixedOnboardingResponse, onboardingResponse] = await Promise.all([
+                this.$axios.get<FixedOnboardingPage, { data: FixedOnboardingPage[] }>('/api/onboarding/fixed'),
+                this.$axios.get<OnboardingPage, { data: OnboardingPage[] }>('/api/onboarding')
+            ])
                 .catch((err) => {
                     this.$toast.error('Could not get onboarding pages');
                     console.error(err);
-                    return null;
+                    return [{ data: [] }, { data: [] }];
                 });
-
-            if (response !== null) {
-                this.updateOnboardingPages(response.data);
-            }
+            this.updateFixedOnboardingPages(fixedOnboardingResponse.data);
+            this.updateOnboardingPages(onboardingResponse.data);
         },
-        // Updates the current pages state with new data
+        // Updates the current onboardingPages state with new data
         updateOnboardingPages(onboardingPages: OnboardingPage[]) {
             const newPageMap = new Map(onboardingPages.map(key => [key.id, key]));
             const updatedPages = this.onboardingPages.map(page => newPageMap.get(page.id) || page);
@@ -52,14 +100,25 @@ export const useOnboardingStore = defineStore('onboarding', {
             });
             this.onboardingPages = updatedPages.filter(page => newPageMap.has(page.id)).sort((a, b) => a.order - b.order);
         },
-        async updateOnboardingPage(onboardingPage: Pick<OnboardingPage, 'id'> & Partial<OnboardingPage>) {
+        // Updates the current fixedOnboardingPages state with new data
+        updateFixedOnboardingPages(onboardingPages: FixedOnboardingPage[]) {
+            const newPageMap = new Map(onboardingPages.map(key => [key.id, key]));
+            const updatedPages = this.onboardingPages.map(page => newPageMap.get(page.id) || page);
+            newPageMap.forEach((page, id) => {
+                if (!this.onboardingPages.some(k => k.id === id)) {
+                    updatedPages.push(page);
+                }
+            });
+            this.fixedOnboardingPages = updatedPages.filter(page => newPageMap.has(page.id)).sort((a, b) => a.id - b.id);
+        },
+        async updateOnboardingPage(onboardingPage: Pick<OnboardingPage, 'id'> & Partial<OnboardingPage>, fixed = false) {
             const formData = new FormData();
             Object.keys(onboardingPage).forEach((key) => {
                 // @ts-ignore
                 formData.append(key, onboardingPage[key]);
             });
             await this.$axios
-                .put<OnboardingPage>(`/api/onboarding/${onboardingPage.id}`, formData, { disableErrorToast: true })
+                .put<OnboardingPage>(`/api/onboarding/${fixed ? 'fixed/' : ''}${onboardingPage.id}`, formData, { disableErrorToast: true })
                 .catch((err) => {
                     this.$toast.error('Could not update onboarding page');
                     console.error(err);
---
+++ apps/wizarr-frontend/src/types/api/onboarding/fixedOnboardingPage.ts
@@ -0,0 +1,10 @@
+/**
+ * Interface for the Fixed Onboarding Page page
+ * @interface FixedOnboardingPage
+ * @property {number} id - The id of the fixed onboarding page
+ * @property {string} value - The content for the fixed onboarding page
+ */
+export interface FixedOnboardingPage {
+    id: number;
+    value: string;
+}
