f2ac26a4af130309e6b2c385b3e696f2f446ac4a
albinmedoc <stamline203@gmail.com>
2024-08-03 23:49:27 +0200

    feat: 🎉 Add MAX_CONTENT_LENGTH configuration for file uploads

--- apps/wizarr-backend/wizarr_backend/definitions.py
+++ apps/wizarr-backend/wizarr_backend/definitions.py
@@ -3,3 +3,4 @@ from os import path, getenv
 ROOT_DIR = getenv("ROOT_DIR", path.abspath(path.join(path.abspath(__file__), "../")))
 LATEST_FILE = getenv("LATEST_FILE", path.join(ROOT_DIR, "../", "../", "../", "latest"))
 DATABASE_DIR = getenv("DATABASE_DIR", path.join(ROOT_DIR, "../", "database"))
+MAX_CONTENT_LENGTH = getenv("MAX_CONTENT_LENGTH", 5 * 1024 * 1024) # 5MB
