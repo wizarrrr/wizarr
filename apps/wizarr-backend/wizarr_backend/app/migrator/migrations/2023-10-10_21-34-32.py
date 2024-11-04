fe34ab835487c61837763e9a4f3189c6a44cfdd3
depfryer <maxime.leblond76@gmail.com>
2024-11-04 06:35:23 +0100

    add support for postgresql

--- apps/wizarr-backend/wizarr_backend/app/migrator/migrations/2023-10-10_21-34-32.py
+++ apps/wizarr-backend/wizarr_backend/app/migrator/migrations/2023-10-10_21-34-32.py
@@ -9,13 +9,17 @@ from playhouse.migrate import *
 from datetime import datetime
 
 from app import db
+from os import environ
 
 # Do not change the name of this file,
 # migrations are run in order of their filenames date and time
 
 def run():
     # Use migrator to perform actions on the database
-    migrator = SqliteMigrator(db)
+    if environ.get('POSTGRES_ENABLED', 'false').lower() == 'true':
+       migrator = PostgresqlMigrator(db)
+    else:
+        migrator = SqliteMigrator(db)
 
     # Add duration column to the invitations table with a default value of null
     # if the column already exists, do nothing
