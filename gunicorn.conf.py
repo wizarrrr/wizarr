3a9fd726b099703b235e5f80d0c278cb580e6cf5
Matthieu B <matthieu@jaseroque.com>
2025-05-23 22:51:16 +0100

    fix libraries

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,9 +3,11 @@ from app.extensions import scheduler
 from app.scripts.migrate_libraries import migrate_libraries
 
 def on_starting(server):
-    
-    migrate_libraries()
+
     # this runs once, in the Gunicorn master
     app = create_app()
+
+    migrate_libraries(app)
+    
     scheduler.init_app(app)
     scheduler.start()
