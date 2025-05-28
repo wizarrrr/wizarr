84a602232fecf55c3d711592427e451801b672be
Jandalslap <128538609+Jandalslap@users.noreply.github.com>
2025-05-28 20:47:48 +1200

    Added new blueprint status
    Created a new blueprint called status to enable an api endpoint /api/status for a widget.
    Queries database and outputs JSON.
    Requires an API Key else is not accessible.

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -7,6 +7,7 @@ from .wizard.routes  import wizard_bp
 from .plex.routes import plex_bp
 from .notifications.routes import notify_bp
 from .jellyfin.routes import jellyfin_bp
+from .status.routes import status_bp 
 
 all_blueprints = (public_bp, wizard_bp, admin_bp, auth_bp,
-                  settings_bp, setup_bp, plex_bp, notify_bp, jellyfin_bp)
+                  settings_bp, setup_bp, plex_bp, notify_bp, jellyfin_bp, status_bp)
