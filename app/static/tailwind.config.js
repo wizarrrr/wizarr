6a94e1d5f99892beca4b2288e4e500e078c9ea70
fireph <443370+fireph@users.noreply.github.com>
2025-06-04 06:17:18 -0700

    fix so that theme works again

--- app/static/tailwind.config.js
+++ app/static/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: 'class',
     content: ["../templates/**/*.html",
         "../templates/*.html",
         "./node_modules/flowbite/**/*.js",
