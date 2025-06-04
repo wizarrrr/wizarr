f17e6fdad30ebc801ba4fd12474c530a21f24346
fireph <443370+fireph@users.noreply.github.com>
2025-06-03 20:19:58 -0700

    Remove node_modules and add it to gitignore

--- app/static/node_modules/alpinejs/builds/module.js
+++
@@ -1,3 +0,0 @@
-import Alpine from './../src/index'
-
-export default Alpine
--- app/static/node_modules/flowbite/lib/cjs/dom/events.d.ts
+++
@@ -1,8 +0,0 @@
-declare class Events {
-    private _eventType;
-    private _eventFunctions;
-    constructor(eventType: string, eventFunctions?: EventListener[]);
-    init(): void;
-}
-export default Events;
-//# sourceMappingURL=events.d.ts.map
