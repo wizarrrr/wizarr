4b32c5285a7a301c4d4832e27aa8f3a7256a1cde
Matthieu B <matthieu@jaseroque.com>
2025-07-11 15:29:52 +0200

    added translation strings

--- app/forms/settings.py
+++ app/forms/settings.py
@@ -2,30 +2,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Optional, URL
+from flask_babel import lazy_gettext as _l
 
 
 class SettingsForm(FlaskForm):
     server_type   = SelectField(
-        "Server Type",
+        _l("Server Type"),
         choices=[("plex", "Plex"), ("jellyfin", "Jellyfin"), ("emby", "Emby"), ("audiobookshelf", "Audiobookshelf"), ("romm", "Romm"), ("komga", "Komga")],
         validators=[DataRequired()],
     )
-    server_name   = StringField("Server Name",   validators=[DataRequired()])
-    server_url    = StringField("Server URL",    validators=[DataRequired()])
-    api_key       = StringField("API Key",       validators=[Optional()])
-    server_username = StringField("RomM Username", validators=[Optional()])
-    server_password = StringField("RomM Password", validators=[Optional()])
-    libraries     = StringField("Libraries",     validators=[Optional()])
-    allow_downloads_plex = BooleanField("Allow Downloads", default=False, validators=[Optional()])
-    allow_tv_plex = BooleanField("Allow Live TV", default=False, validators=[Optional()])
-    overseerr_url = StringField("Overseerr/Ombi URL", validators=[Optional(), URL()])
-    ombi_api_key  = StringField("Ombi API Key",  validators=[Optional()])
-    discord_id    = StringField("Discord ID",    validators=[Optional()])
-    external_url  = StringField("External URL", validators=[Optional()])
+    server_name   = StringField(_l("Server Name"),   validators=[DataRequired()])
+    server_url    = StringField(_l("Server URL"),    validators=[DataRequired()])
+    api_key       = StringField(_l("API Key"),       validators=[Optional()])
+    server_username = StringField(_l("RomM Username"), validators=[Optional()])
+    server_password = StringField(_l("RomM Password"), validators=[Optional()])
+    libraries     = StringField(_l("Libraries"),     validators=[Optional()])
+    allow_downloads_plex = BooleanField(_l("Allow Downloads"), default=False, validators=[Optional()])
+    allow_tv_plex = BooleanField(_l("Allow Live TV"), default=False, validators=[Optional()])
+    overseerr_url = StringField(_l("Overseerr/Ombi URL"), validators=[Optional(), URL()])
+    ombi_api_key  = StringField(_l("Ombi API Key"),  validators=[Optional()])
+    discord_id    = StringField(_l("Discord ID"),    validators=[Optional()])
+    external_url  = StringField(_l("External URL"), validators=[Optional()])
     
     # Jellyfin specific
-    allow_downloads_jellyfin = BooleanField("Allow Downloads", default=False, validators=[Optional()])
-    allow_tv_jellyfin       = BooleanField("Allow Live TV", default=False, validators=[Optional()])
+    allow_downloads_jellyfin = BooleanField(_l("Allow Downloads"), default=False, validators=[Optional()])
+    allow_tv_jellyfin       = BooleanField(_l("Allow Live TV"), default=False, validators=[Optional()])
 
     def __init__(self, install_mode: bool = False, *args, **kwargs):
         super().__init__(*args, **kwargs)
