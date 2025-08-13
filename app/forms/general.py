612931a743239fd8aa5630686660455d31c91dd3
Matthieu B <matthieu@jaseroque.com>
2025-08-13 22:11:38 +0200

    Remove external integration fields from GeneralSettingsForm and associated HTML template

--- app/forms/general.py
+++ app/forms/general.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField
-from wtforms.validators import URL, DataRequired, Optional
+from wtforms.validators import DataRequired, Optional
 
 
 class GeneralSettingsForm(FlaskForm):
@@ -8,8 +8,3 @@ class GeneralSettingsForm(FlaskForm):
     wizard_acl_enabled = BooleanField(
         "Protect Wizard Access", default=True, validators=[Optional()]
     )
-
-    # External Integration Settings
-    overseerr_url = StringField("Overseerr/Ombi URL", validators=[Optional(), URL()])
-    ombi_api_key = StringField("Ombi API Key", validators=[Optional()])
-    discord_id = StringField("Discord ID", validators=[Optional()])
