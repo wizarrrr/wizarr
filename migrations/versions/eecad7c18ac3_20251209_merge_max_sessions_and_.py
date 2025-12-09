6758372bb3e6a5135f6c69b554eceedc4e285319
Matthieu B <matthieu@jaseroque.com>
2025-12-09 14:08:42 +0100

    fix: merge migrations

---
+++ migrations/versions/eecad7c18ac3_20251209_merge_max_sessions_and_.py
@@ -0,0 +1,21 @@
+"""20251209_merge_max_sessions_and_password_reset
+
+Revision ID: eecad7c18ac3
+Revises: 080eaac6e013, c854ad44aad5
+Create Date: 2025-12-09 14:07:46.925765
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "eecad7c18ac3"
+down_revision = ("080eaac6e013", "c854ad44aad5")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
